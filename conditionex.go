f6174633a252d32b8d16c0c68522b15a73a2a397
Bishalkl <Bishal123koirala@gmail.com>
2025-02-23 17:55:06 +0545

    done

--- conditionex.go
+++ conditionex.go
@@ -42,24 +42,60 @@ func main() {
 	// 	fmt.Println("it is a negative number.")
 	// }
 
-	var age int
+	// var age int
 
-	// asking the age 
-	fmt.Print("Please enter your  age: ")
+	// // asking the age
+	// fmt.Print("Please enter your  age: ")
 
-	// Check for valid input
-	if _, err := fmt.Scanln(&age); err != nil {
-		fmt.Println("Invalid input: Please input valid age.")
-		return 
-	}
+	// // Check for valid input
+	// if _, err := fmt.Scanln(&age); err != nil {
+	// 	fmt.Println("Invalid input: Please input valid age.")
+	// 	return
+	// }
 
-	// check for valid age 
-	if age >= 18 {
-		fmt.Println("Yes, you are eligible!")
-	} else {
-		fmt.Println("No, you are not eligible!")
-	}
+	// // check for valid age
+	// if age >= 18 {
+	// 	fmt.Println("Yes, you are eligible!")
+	// } else {
+	// 	fmt.Println("No, you are not eligible!")
+	// }
 
+	// var num1, num2, num3 int
 
+	// // ask for input
+	// fmt.Print("Please enter the first number: ")
+
+	// // check for vaild input
+	// if _, err := fmt.Scanln(&num1); err != nil {
+	// 	fmt.Println("Invalid input: Please enter a valid number.")
+	// 	return
+	// }
+
+	// fmt.Print("Please enter the  second number: ")
+
+	// // check for vaild input
+	// if _, err := fmt.Scanln(&num2); err != nil {
+	// 	fmt.Println("Invalid input: Please enter a valid number.")
+	// 	return
+	// }
+
+	// fmt.Print("Please enter the third number: ")
+
+	// // check for vaild input
+	// if _, err := fmt.Scanln(&num3); err != nil {
+	// 	fmt.Println("Invalid input: Please enter a valid number.")
+	// 	return
+	// }
+
+	// // check for which is largest
+	// if num1 == num2 && num1 == num3 {
+	// 	fmt.Println("All numbers are equal.")
+	// } else if num1 > num2 && num1 > num3 {
+	// 	fmt.Println("The first number is the largest.")
+	// } else if num2 > num1 && num2 > num3 {
+	// 	fmt.Println("The second number is the largest.")
+	// } else {
+	// 	fmt.Println("The third number is the largest.")
+	// }
 
 }
