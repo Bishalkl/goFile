5c5618755166962cb775d0a50d7819dba21ee83b
Bishalkl <Bishal123koirala@gmail.com>
2025-02-24 13:11:28 +0545

    done

--- funcDeferex.go
+++ funcDeferex.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Write a function that takes two numbers and returns their sum, difference, product, and quotient as multiple return values.
 
@@ -16,16 +19,44 @@ func operation(a, b int) (sum int, diff int, product int, quotient int, err erro
 	return
 }
 
-func main() {
-	a, b := 10, 2
-	sum, diff, product, quotient, err := operation(a, b)
-
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Sum:", sum)
-		fmt.Println("Difference:", diff)
-		fmt.Println("Product:", product)
-		fmt.Println("Quotient:", quotient)
+func factorial(num int) (int, error) {
+	if num < 0 {
+		return 0, errors.New("Factorial is not defined for negative numbers")
 	}
+
+	if num == 0 || num == 1 {
+		return 1, nil
+	}
+
+	// Recursive call
+	fact, err := factorial(num - 1)
+	if err != nil {
+		return 0, err
+	}
+
+	return num * fact, nil
+
+}
+
+// write a function that accepts a variable number of integer arguments (variadic function) and return their sum.
+
+func sum(numbers ...int) int {
+	var total int
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+// Write a function that swaps two integer using pointers in Go.
+func swapNum(a, b *int) {
+	*a, *b = *b, *a
+}
+
+func main() {
+	x, y := 10, 20
+	fmt.Println("Before swapping:", x, y) // Output: Before swapping: 10 20
+
+	swapNum(&x, &y)                      // Passing addresses
+	fmt.Println("After swapping:", x, y) // Output: After swapping: 20 10
 }
